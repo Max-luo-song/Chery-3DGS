26ffcdd1f1dbee588b48aa08fd66749e5393ae0a
yangtao27 <yangtao27@mychery.com>
2025-10-09 14:40:26 +0800

    [scene][NA-0]adapt
    Signed-off-by: yangtao27 <yangtao27@mychery.com>

--- lidargs/_exp/render.sh
+++ lidargs/_exp/render.sh
@@ -1,13 +1,9 @@
-iterations=4000
-output_dir="./output"
-gpu=2
-
-casename="waymo"
-data="/data/sfs_turbo/yangtao/waymo_train_small" # Replace it with the path where you unzipped the file
-
-caseid="segment-1005081002024129653_5313_150_5333_150_with_camera_labels"
-logdir="$casename/$caseid"
+iterations=2000
+gpu=0
+data="/home/not0513/data/orinY/processed/training/20250702_133223_Q2517" # Replace it with the path where you unzipped the file
+output_dir="/home/not0513/data/orinY/processed/test/20250702_133223_Q2517"
+caseid="20250702_133223_Q2517"
 sensorid=0
-python3 render.py -s ${data} -m ${output_dir}/${logdir} --caseid ${caseid} --iteration ${iterations} --max_depth 80 --blockinfo ${output_dir}/${logdir}
+python3 render.py -s ${data} -m ${output_dir} --caseid ${caseid} --iteration ${iterations} --max_depth 80 --blockinfo ${output_dir}
 
 
