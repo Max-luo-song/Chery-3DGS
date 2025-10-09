5ac4a5b45511c51579f27c3223cd66f36a114f45
yangtao27 <yangtao27@mychery.com>
2025-10-09 14:40:26 +0800

    [scene][NA-0]adapt
    Signed-off-by: yangtao27 <yangtao27@mychery.com>

--- lidargs/_exp/triain_waymo1.sh
+++ lidargs/_exp/triain_waymo1.sh
@@ -1,16 +1,10 @@
 
-# pip install submodules/diff_lidargs_surfel_rasterization
-# path="/home/xuanyuan/test2/LiDAR_GS(Waymo)" # # Replace it with the workspace path
-iterations=4000
-output_dir="/data/sfs_turbo/yangtao/output"
-gpu=2
-
-casename="waymo"
-data="/data/sfs_turbo/yangtao/data/waymo_train_small" # Replace it with the path where you unzipped the file
-
-caseid="segment-1005081002024129653_5313_150_5333_150_with_camera_labels"
-logdir="$casename/$caseid"
+iterations=2000
+gpu=0
+data="/home/not0513/data/orinY/processed/training/20250702_133223_Q2517" # Replace it with the path where you unzipped the file
+output_dir="/home/not0513/data/orinY/processed/test/20250702_133223_Q2517"
+caseid="20250702_133223_Q2517"
 sensorid=0
-python3 train.py -s ${data} --caseid ${caseid} --gpu ${gpu} --iterations ${iterations} -m ${output_dir}/${logdir} --max_depth 80
+python3 train.py -s ${data} --caseid ${caseid} --gpu ${gpu} --iterations ${iterations} -m ${output_dir} --max_depth 200
 
 
