9f0c3a321cc145509f9cc183e571d4b1cfe656f9
ycshao21 <ycshao21@gmail.com>
2025-11-11 16:07:37 +0800

    Human pose support for QCraft

--- datasets/tools/humanpose_process.py
+++ datasets/tools/humanpose_process.py
@@ -10,16 +10,17 @@ from datasets.tools.postprocess import match_and_postprocess
 
 logger = logging.getLogger()
 
+
 def extract_humanpose(
     scene_dir,
     projection_fn: Callable,
     camera_list: List[str],
-    save_temp: bool=True,
-    verbose: bool=False,
-    fps: int=12
+    save_temp: bool = True,
+    verbose: bool = False,
+    fps: int = 12,
 ):
     """Extract human pose from the waymo dataset
-    
+
     Args:
         scene_dir: str, path to the scene directory
         save_temp: bool, whether to save the intermediate results
@@ -28,32 +29,42 @@ def extract_humanpose(
     """
     # project human boxes to 2D image space
     GTTracks_meta = projection_fn(
-        scene_dir, camera_list=camera_list,
-        save_temp=save_temp, verbose=verbose,
-        narrow_width_ratio=0.2, fps=fps
+        scene_dir,
+        camera_list=camera_list,
+        save_temp=save_temp,
+        verbose=verbose,
+        narrow_width_ratio=0.2,
+        fps=fps,
     )
-    
+
     # run 4DHuman to get predicted human tracks with SMPL parameters
     PredTracks_meta = run_4DHumans(
-        scene_dir, camera_list=camera_list,
-        save_temp=save_temp, verbose=verbose, fps=fps
+        scene_dir,
+        camera_list=camera_list,
+        save_temp=save_temp,
+        verbose=verbose,
+        fps=fps,
     )
-    
+
     # match the predicted tracks with the ground truth tracks
     smpl_meta = match_and_postprocess(
-        scene_dir, camera_list=camera_list,
-        GTTracksDict=GTTracks_meta, PredTracksDict=PredTracks_meta,
-        save_temp=save_temp, verbose=verbose, fps=fps
+        scene_dir,
+        camera_list=camera_list,
+        GTTracksDict=GTTracks_meta,
+        PredTracksDict=PredTracks_meta,
+        save_temp=save_temp,
+        verbose=verbose,
+        fps=fps,
     )
-    
-    joblib.dump(
-        smpl_meta,
-        os.path.join(scene_dir, "humanpose", "smpl.pkl")
-    )
+
+    joblib.dump(smpl_meta, os.path.join(scene_dir, "humanpose", "smpl.pkl"))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Data converter arg parser")
-    parser.add_argument("--data_root", type=str, required=True, help="root path of waymo dataset")
+    parser.add_argument(
+        "--data_root", type=str, required=True, help="root path of waymo dataset"
+    )
     parser.add_argument("--dataset", type=str, default="waymo", help="dataset name")
     parser.add_argument(
         "--scene_ids",
@@ -94,22 +105,39 @@ if __name__ == "__main__":
         help="FPS for the visualization video if verbose is True",
     )
     args = parser.parse_args()
-    
-    if args.dataset == "waymo":
+
+    if args.dataset == "qcraft":
+        from datasets.qcraft.qcraft_human_utils import project_human_boxes, CAMERA_LIST
+    elif args.dataset == "chery":
+        # TODO(syc)
+        # from datasets.chery.chery_human_utils import project_human_boxes, CAMERA_LIST
+        pass
+    elif args.dataset == "waymo":
         from datasets.waymo.waymo_human_utils import project_human_boxes, CAMERA_LIST
     elif args.dataset == "pandaset":
-        from datasets.pandaset.pandaset_human_utils import project_human_boxes, CAMERA_LIST
+        from datasets.pandaset.pandaset_human_utils import (
+            project_human_boxes,
+            CAMERA_LIST,
+        )
     elif args.dataset == "argoverse":
-        from datasets.argoverse.argoverse_human_utils import project_human_boxes, CAMERA_LIST
+        from datasets.argoverse.argoverse_human_utils import (
+            project_human_boxes,
+            CAMERA_LIST,
+        )
     elif args.dataset == "nuscenes":
-        from datasets.nuscenes.nuscenes_human_utils import project_human_boxes, CAMERA_LIST
+        from datasets.nuscenes.nuscenes_human_utils import (
+            project_human_boxes,
+            CAMERA_LIST,
+        )
     elif args.dataset == "kitti":
         from datasets.kitti.kitti_human_utils import project_human_boxes, CAMERA_LIST
     elif args.dataset == "nuplan":
         from datasets.nuplan.nuplan_human_utils import project_human_boxes, CAMERA_LIST
     else:
-        raise ValueError(f"Unknown dataset {args.dataset}, please choose from waymo, pandaset, argoverse, nuscenes, kitti, nuplan")
-    
+        raise ValueError(
+            f"Unknown dataset {args.dataset}, please choose from waymo, pandaset, argoverse, nuscenes, kitti, nuplan"
+        )
+
     if args.scene_ids is not None:
         scene_ids_list = args.scene_ids
     elif args.split_file is not None:
@@ -123,17 +151,17 @@ if __name__ == "__main__":
             scene_ids_list = [line.strip().split(" ")[0] for line in split_file]
     else:
         scene_ids_list = np.arange(args.start_idx, args.start_idx + args.num_scenes)
-    
+
     for scene_id in scene_ids_list:
         try:
-            scene_dir = f'{args.data_root}/{str(scene_id).zfill(3)}'
+            scene_dir = f"{args.data_root}/{str(scene_id).zfill(3)}"
             extract_humanpose(
                 scene_dir=scene_dir,
                 projection_fn=project_human_boxes,
                 camera_list=CAMERA_LIST,
                 save_temp=args.save_temp,
                 verbose=args.verbose,
-                fps=args.fps
+                fps=args.fps,
             )
             logger.info(f"Finished processing scene {scene_id}")
         except Exception as e:
