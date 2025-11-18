d6b568421a017d1e5cf472ac335c8522c9bc187c
baitongyao <baitongyao@mychery.com>
2025-11-18 15:10:35 +0800

    [scene][NA-0][feat] add mix_novel_views

--- scripts/qcraft/preprocess_data.sh
+++ scripts/qcraft/preprocess_data.sh
@@ -5,11 +5,11 @@ scene_id="20251105_152839_QCOYSD504206_1240_1255"
 
 export PYTHONPATH=$(pwd)
 python datasets/preprocess.py \
-    --data_root data/qcraft/raw/ \
-    --target_dir data/qcraft/processed \
+    --data_root /nas_thoru/oldbak/baitongyao/qcraft/raw/ \
+    --target_dir /nas_thoru/oldbak/baitongyao/qcraft/processed \
     --dataset qcraft \
     --split training \
     --scene_ids $scene_id \
     --workers 2 \
-    --process_keys ego_masks images calib pose objects dynamic_masks lidar \
+    --process_keys ego_masks images calib pose objects dynamic_masks lidar mix_novel_views\
     --skip_front_wide_side_cameras
