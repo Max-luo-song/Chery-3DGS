98392e11a5f94a50acd0cb5fe5ccc9976effd2b2
Max-luo-song <1394286257@qq.com>
2025-12-09 20:38:10 +0800

    add depth_loss

--- sim_render/cam/render_novel_trajectory.sh
+++ sim_render/cam/render_novel_trajectory.sh
@@ -21,8 +21,8 @@ traj_types=(
     # change_lane_3.5m
 )
 
-cam_ids=(0 1 2 3 4 5 6 7 8 9 10 11 12)
-downscales=(1 1 1 1 1 1 1 1 1 1 1 1 1)
+cam_ids=(0 1 2 3 5 6 7 9 10 11 12)
+downscales=(1 1 1 1 1 1 1 1 1 1 1)
 
 fps=10
 
