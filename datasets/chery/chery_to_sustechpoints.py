805793abcae04909e4c20770db24b97bcb36cdb3
baitongyao <baitongyao@mychery.com>
2025-12-15 17:30:07 +0800

    [scene][NA-0][fix] fix lidar trans bug

--- datasets/chery/chery_to_sustechpoints.py
+++ datasets/chery/chery_to_sustechpoints.py
@@ -2,30 +2,26 @@ import numpy as np, argparse, os, shutil, json
 import open3d as o3d
 from typing import Dict, List
 from scipy.spatial.transform import Rotation as R
-
-OPENCV2DATASET = np.array(
-    [
-        [0.0, 0.0, 1.0, 0.0],
-        [-1.0, 0.0, 0.0, 0.0],
-        [0.0, -1.0, 0.0, 0.0],
-        [0.0, 0.0, 0.0, 1.0],
-    ]
+from datasets.qcraft.qcraft_helpers import (
+    OPENCV2DATASET,
+    euler_to_transform_matrix,
 )
+from chery_tools.parse_lidar import parse_lidar_pcd_file
 
 CAMERA_PERSPECTIVE = [
-    # "CAM_PBQ_FRONT_LEFT_RESET_OPTICAL_H99",
-    # "CAM_PBQ_FRONT_RIGHT_RESET_OPTICAL_H99",
-    "CAM_PBQ_FRONT_TELE_RESET_OPTICAL_H15",
-    "CAM_PBQ_FRONT_TELE_RESET_OPTICAL_H30",
+    "CAM_PBQ_FRONT_WIDE_RESET_OPTICAL_H110",  # 车内
     "CAM_PBQ_FRONT_WIDE_RESET_OPTICAL_H60",
-    # "CAM_PBQ_FRONT_WIDE_RESET_OPTICAL_H110",  # 车内
-    # "CAM_PBQ_FRONT_WIDE_RESET_OPTICAL_LEFT_H60",  # 车内
-    # "CAM_PBQ_FRONT_WIDE_RESET_OPTICAL_RIGHT_H60",  # 车内
-    # "CAM_PBQ_REAR_LEFT_RESET_OPTICAL_H30",  # 分辨率小
-    # "CAM_PBQ_REAR_RIGHT_RESET_OPTICAL_H30",  # 分辨率小
-    # "CAM_PBQ_REAR_RESET_OPTICAL_H50",
-    # "CAM_PBQ_REAR_LEFT_RESET_OPTICAL_H99",
-    # "CAM_PBQ_REAR_RIGHT_RESET_OPTICAL_H99",
+    "CAM_PBQ_FRONT_TELE_RESET_OPTICAL_H30",
+    "CAM_PBQ_FRONT_TELE_RESET_OPTICAL_H15",
+    # "CAM_PBQ_FRONT_WIDE_RESET_OPTICAL_LEFT_H60",
+    "CAM_PBQ_FRONT_LEFT_RESET_OPTICAL_H99",
+    "CAM_PBQ_REAR_LEFT_RESET_OPTICAL_H99",
+    "CAM_PBQ_REAR_LEFT_RESET_OPTICAL_H30",
+    # "CAM_PBQ_FRONT_WIDE_RESET_OPTICAL_RIGHT_H60",
+    "CAM_PBQ_FRONT_RIGHT_RESET_OPTICAL_H99",
+    "CAM_PBQ_REAR_RIGHT_RESET_OPTICAL_H99",
+    "CAM_PBQ_REAR_RIGHT_RESET_OPTICAL_H30",
+    "CAM_PBQ_REAR_RESET_OPTICAL_H50",
 ]
 
 
@@ -69,6 +65,7 @@ def euler2transform(chery_extrinsics: Dict, type) -> List:
     T = np.eye(4)
     if type == "camera":
         R_mat = np.array([[0, -1, 0], [0, 0, -1], [1, 0, 0]]).T @ R_mat
+
     T[:3, :3] = R_mat
     T[:3, 3] = [
         chery_extrinsics.get("x"),
@@ -78,18 +75,21 @@ def euler2transform(chery_extrinsics: Dict, type) -> List:
     return T.tolist()
 
 
-def gen_lidar_to_cams(ego2lidar, cam_exs) -> List:
-    assert isinstance(cam_exs[0], list), "cam_exs shape error"
+def gen_lidar_to_cam(ego2lidar, cam2ego) -> List:
+    assert isinstance(cam2ego, list), "cam_exs shape error"
     assert isinstance(ego2lidar, list), "lidar_ex shape error"
-    return [(np.array(ego2lidar) @ (np.array(c2e) @ OPENCV2DATASET)).tolist() for c2e in cam_exs]
+    return np.linalg.inv(np.array(ego2lidar) @ np.array(cam2ego)).tolist()
+
 
 def transform_lidar(lidar2ego, source_file, dst_file):
     ori_pcd = o3d.io.read_point_cloud(source_file)
-    ori_pts = np.asarray(ori_pcd.points)
+    pc_ego = parse_lidar_pcd_file(source_file)
+    ori_pts = np.array(ori_pcd.points)
     xyz_ego_homo = np.hstack([ori_pts, np.ones((ori_pts.shape[0], 1))])  # [N, 4]
-    xyz_lidar_homo = (np.linalg.inv(lidar2ego) @ xyz_ego_homo.T).T  # [N, 4]
+    xyz_lidar_homo = (np.linalg.inv(lidar2ego) @ xyz_ego_homo.T).T # [N, 4]
     ori_pcd.points = o3d.utility.Vector3dVector(xyz_lidar_homo[:, :3])
     o3d.io.write_point_cloud(dst_file, ori_pcd)
+
 
 def organize_meta(args, all_time_stamps, lidar2ego):
     # create dirs
@@ -108,14 +108,13 @@ def organize_meta(args, all_time_stamps, lidar2ego):
                 dst_path = os.path.join(args.dst, "camera", cam_id)
                 if os.path.exists(dst_path):
                     shutil.copy(src_file, os.path.join(dst_path, ts + ".jpg"))
-                # else:
-                #     print(f"{dst_path} not exist, not include cam_id: {cam_id}")
             elif file.endswith(".pcd"):
                 src_file = os.path.join(args.src, ts, file)
                 dst_path = os.path.join(args.dst, "lidar")
                 if os.path.exists(dst_path):
-                    transform_lidar(lidar2ego, src_file, os.path.join(dst_path, ts + ".pcd"))
-                    # shutil.copy(src_file, os.path.join(dst_path, ts + ".pcd"))
+                    transform_lidar(
+                        lidar2ego, src_file, os.path.join(dst_path, ts + ".pcd")
+                    )
 
 
 def organize_calibs(args):
@@ -135,30 +134,44 @@ def organize_calibs(args):
     with open(raw_car_info_file_pth, encoding="utf-8") as f:
         car_infos = json.load(f)
 
-    lidar_ext = euler2transform(
+    lidar_info = (
         car_infos.get("lidar_params", False)[0]
         .get("installation", False)
-        .get("extrinsics", False),
-        "lidar",
+        .get("extrinsics", False)
     )
-    
-    ego2lidar = np.linalg.inv(lidar_ext).tolist()
+
+    lidar2ego = euler_to_transform_matrix(
+        lidar_info["x"],
+        lidar_info["y"],
+        lidar_info["z"],
+        lidar_info["yaw"],
+        lidar_info["pitch"],
+        lidar_info["roll"],
+    )
+
+    ego2lidar = np.linalg.inv(lidar2ego).tolist()
 
     # cal lidar to cam extrinsics
-    lidar2cams = gen_lidar_to_cams(
-        ego2lidar,
-        [
-            euler2transform(
-                cam_params.get(cam_id, False).get(
-                    "camera_to_vehicle_extrinsics", False
-                ),
-                "camera",
+    lidar2cams = {}
+    for cam_id in CAMERA_PERSPECTIVE:
+        if cam_params.get(cam_id, False):
+            caminfo = cam_params.get(cam_id, False).get(
+                "camera_to_vehicle_extrinsics", False
             )
-            for cam_id in CAMERA_PERSPECTIVE
-        ],
-    )
+            cam2ego = euler_to_transform_matrix(
+                caminfo["x"],
+                caminfo["y"],
+                caminfo["z"],
+                caminfo["yaw"],
+                caminfo["pitch"],
+                caminfo["roll"],
+            )
+            cam2ego = cam2ego @ OPENCV2DATASET
+            lidar2cams[cam_id] = gen_lidar_to_cam(ego2lidar, cam2ego.tolist())
 
     for i, cam_id in enumerate(CAMERA_PERSPECTIVE):
+        if not lidar2cams.get(cam_id, False):
+            continue
         dst_file_path = os.path.join(args.dst, "calib/camera", cam_id + ".json")
         intrinsic = [
             cam_params.get(cam_id, False).get("intrinsics", False).get("fx", False),
@@ -172,12 +185,14 @@ def organize_calibs(args):
             1,
         ]
         info = {
-            "extrinsic": np.asarray(lidar2cams[i]).flatten().tolist(),
+            "extrinsic": np.asarray(lidar2cams[cam_id]).flatten().tolist(),
             "intrinsic": intrinsic,
         }
         with open(dst_file_path, "w", encoding="utf-8") as f:
             json.dump(info, f)
-    return lidar_ext
+            print(f"save {dst_file_path}")
+    return lidar2ego
+
 
 def create_dirs(path):
     if os.path.exists(path):
@@ -210,7 +225,6 @@ def convert2sus(args):
 
     lidar2ego = organize_calibs(args)
     organize_meta(args, all_time_stamps_list, lidar2ego)
-    
 
 
 if __name__ == "__main__":
@@ -224,5 +238,5 @@ if __name__ == "__main__":
     convert2sus(args)
 
 """
-python datasets/chery/chery_to_sustechpoints.py --src ~/Downloads/场景重建/qcraft_0702/20250702_133223_Q2517_60_75 --dst ~/Tools/SUSTechPOINTS/data/qcraft_test_1215
+python datasets/chery/chery_to_sustechpoints.py --src ~/Downloads/场景重建/20251104_141412_QLC0R1300042 --dst ~/Tools/SUSTechPOINTS/data/qcraft_test_1104
 """
